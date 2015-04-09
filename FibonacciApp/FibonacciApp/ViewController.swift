e9b0ca322fe9d1054443fe4259b1003889590de9
rfelty <rfelty@gmail.com>
2015-04-08 21:14:47 -0400

    Tweaks to the first working version to update the switch and the slider.

--- FibonacciApp/FibonacciApp/ViewController.swift
+++ FibonacciApp/FibonacciApp/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
+    var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
     @IBAction func updateFibonacciSequence() {
-    fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt(numberOfItemsSlider.value), includesZero: includesZeroSwitch.on)
+        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt(numberOfItemsSlider.value), includesZero: includesZeroSwitch.on)
         textView.text = fibonacciSequence.values.description
+        if includesZeroSwitch.on {
+            includesZeroLabel.text = "Yes"
+        } else {
+            includesZeroLabel.text = "No"
+        }
+        numberOfItemsLabel.text = String(Int(numberOfItemsSlider.value))
     }
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var numberOfItemsSlider: UISlider!
@@ -22,13 +28,13 @@ var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZe
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateFibonacciSequence()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
